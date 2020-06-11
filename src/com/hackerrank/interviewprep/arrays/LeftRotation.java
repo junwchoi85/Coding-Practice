98066094caa74039dc8e13d3cfae1e4212449974
junwchoi85 <42979340+junwchoi85@users.noreply.github.com>
2020-06-11 10:10:00 +0900

    2020.06.11 Coding Practice

--- src/com/hackerrank/interviewprep/arrays/LeftRotation.java
+++ src/com/hackerrank/interviewprep/arrays/LeftRotation.java
@@ -12,16 +12,30 @@ import java.util.regex.*;
 /**
  * url: https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays&h_r=next-challenge&h_v=zen
  */
+/*
+Sample Input0
+5 4
+1 2 3 4 5
+
+Sample Input 1
+20 10
+41 73 89 7 10 1 59 58 84 77 77 97 58 1 86 58 26 10 86 51
+
+Sample Input 2
+15 13
+33 47 70 37 8 53 13 93 71 72 51 100 60 87 97
+
+ */
 public class LeftRotation {
     // Complete the rotLeft function below.
     static int[] rotLeft(int[] a, int d) {
         int l = a.length;
-        int[] result = a;
+        int[] result = new int[l];
         for(int i=(l-d); i<l; i++) {
             result[i] = a[i-(l-d)];
         }
         for(int i=0; i<(l-d); i++) {
-            result[i] = a[l-i];
+            result[i] = a[d+i];
         }
         return result;
     }
---
+++ src/com/hackerrank/interviewprep/arrays/NewYearChaos.java
@@ -0,0 +1,66 @@
+package com.hackerrank.interviewprep.arrays;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+// url: https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays&h_r=next-challenge&h_v=zen
+/*
+Sample Input 0
+2
+5
+2 1 5 3 4
+5
+2 5 1 3 4
+
+Sample Input 1
+2
+8
+5 1 2 3 7 8 6 4
+8
+1 2 5 3 7 8 6 4
+
+Sample Input 2
+1
+8
+1 2 5 3 4 7 8 6
+ */
+public class NewYearChaos {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+        
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
+
+            minimumBribes(q);
+        }
+
+        scanner.close();
+    }
+
+}
