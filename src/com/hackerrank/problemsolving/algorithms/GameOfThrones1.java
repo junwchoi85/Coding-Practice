3934fea698ee60c1b8a1a2dd83112fd5dff36665
junwchoi85 <42979340+junwchoi85@users.noreply.github.com>
2020-06-14 17:07:18 +0900

    Update GameOfThrones1.java

--- src/com/hackerrank/problemsolving/algorithms/GameOfThrones1.java
+++ src/com/hackerrank/problemsolving/algorithms/GameOfThrones1.java
@@ -1,12 +1,7 @@
-package com.hackerrank.problemsolving.Algorithms;
+package com.hackerrank.problemsolving.algorithms;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 /**
  * PROBLEM
